c251c34c9c42242ff2ce1f554cb50043bdb737a8
username-naz <duttaaniruddha2001@gmail.com>
2022-04-06 19:38:50 +0530

    ADD: Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
-import { FaHamburger } from "react-icons/fa";
+import { MdMenuOpen, MdMenu } from "react-icons/md";
+import {BiLogOut} from "react-icons/bi";
+import { useState } from "react";
+
 
 const Navbar = () => {
-
+  const [open, setOpen] = useState(false);
+  const closeMen =  ()=> {setOpen(false)};
+  const openMen =  ()=> {setOpen(true)};
 
   return (
-  <nav>
-    <FaHamburger className="ham-icon"/>
+  <nav className={open?'nav-open':'nav-closed'}>
+    {open && <MdMenuOpen className="ham-icon" onClick={()=>{closeMen()}}/>}
+    {!open && <MdMenu className="ham-icon" onClick={()=>{openMen()}}/>}
     <Link to="#">Orders</Link>
     <Link to="#">Profile</Link>
     <Link to="#">Contact Us</Link>
-    <Link to="#">Log out</Link>
+    <Link to="#" name="logout"><BiLogOut/> &ensp; Log out</Link>
+    {open && <div className="nav-back" onClick={()=>{closeMen()}}></div>}
   </nav>
   );
 };
