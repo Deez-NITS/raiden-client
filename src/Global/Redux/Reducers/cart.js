ad8d74140de321485b7274d78b180778768cdfbc
Pratik Majumdar (codadept) <info.pratikmajumdar@gmail.com>
2022-04-09 01:19:44 +0530

    ADD: Cart redux store add

---
+++ src/Global/Redux/Reducers/cart.js
@@ -0,0 +1,96 @@
+/**
+ * @typedef {[{
+ *      items: [{
+ *        id: number;
+ *        provider: number;
+ *        name: string;
+ *        description: string;
+ *        price: number;
+ *        quantity: number;
+ *      }];
+ *      totalQuantity: number;
+ *      totalPrice: number;
+ *    }]} authState
+ *
+ * @typedef {{
+ *      type: string;
+ *      payload: any;
+ *    }} actionType
+ */
+
+/**
+ * @param { authState } state
+ * @param { actionType } action
+ *
+ * @returns {authState}
+ */
+const addToCart = (state, action) => {
+  const userItem = action.payload;
+
+  const updatedItems = [];
+
+  const foundItem = state.items.find((item) => item.id === userItem.id);
+
+  if (foundItem) {
+    foundItem.quantity++;
+    updatedItems.push(foundItem);
+  } else {
+    updatedItems.push(userItem);
+  }
+
+  updatedItems.push(...state.items);
+
+  const updatedState = {
+    totalQuantity: state.totalQuantity++,
+    totalPrice: state.totalPrice + userItem.price,
+    item: updatedItems,
+  };
+
+  localStorage.setItem("cart", updatedState);
+
+  return updatedState;
+};
+
+/**
+ * @param { authState } state
+ * @param { actionType } action
+ *
+ * @returns {authState}
+ */
+const removeFromCart = (state, action) => {
+  const foundItem = state.items.find((item) => item.id === action.payload);
+
+  const updatedItems = [];
+  let totalPrice = state.totalPrice;
+  let totalQuantity = state.totalQuantity;
+
+  if (foundItem) {
+    totalPrice = state.totalPrice - foundItem.price;
+    totalQuantity = state.totalQuantity--;
+    if (foundItem.quantity === 1) {
+      updatedItems.push(
+        ...state.items.filter((item) => item.id !== foundItem.id)
+      );
+    } else {
+      foundItem.quantity--;
+      updatedItems.push(...state.items, foundItem);
+    }
+  } else {
+    updatedItems.push(...state.items);
+  }
+
+  const updatedState = {
+    totalPrice,
+    totalQuantity,
+    items: updatedItems,
+  };
+
+  localStorage.setItem("cart", updatedState);
+
+  return updatedState;
+};
+
+export default {
+  addToCart,
+  removeFromCart,
+};
