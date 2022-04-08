fc565f77607f9fd539abe28156eefa819a082651
username-naz <duttaaniruddha2001@gmail.com>
2022-04-08 14:19:59 +0530

    ADD: Date select

--- src/Pages/Flight/FlightResults.jsx
+++ src/Pages/Flight/FlightResults.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import { AiFillStar } from "react-icons/ai"
+
 const FlightResults = ({flight})=>{
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
     const airports = [
         {
             name:"Blaze Airport",
@@ -11,34 +18,57 @@ const FlightResults = ({flight})=>{
             rating:"3",
             code:"GHY"
         },
-    ]
+    ];
+
+    const dates = [];
+    const [activeDate, setActiveDate] = useState(0);
+
+    for(let i=0, date = new Date();i<15;i++){
+        dates.push({date:date.getDate(), month:monthNames[date.getMonth()], active:false});
+        date.setDate(date.getDate() + 1);
+    }
+
+    dates[activeDate].active = true;
+
 
     return(
-        <div className="searchResults">
-            <div className="header">
-                <h1>Search Results</h1>
-                <h3>Flight {flight}</h3>
+        <>
+            <div className="searchResults">
+                <div className="header">
+                    <h1>Search Results</h1>
+                    <h3>Flight {flight}</h3>
+                </div>
+
+                <div className="airportList">
+                    {airports.map((airport)=>{
+                        return (
+                            <div className="airport">
+                            <span className="name">
+                            {airport.name}
+                            <span className="rating">
+                            <AiFillStar/> {airport.rating}
+                            </span>
+                            </span>
+                        
+                            <span className="code">
+                            Code: {airport.code}
+                            </span>
+                        
+                            </div>)
+                    })}
+                </div>
             </div>
 
-            <div className="airportList">
-                {airports.map((airport)=>{
-                    return (
-                        <div className="airport">
-                        <span className="name">
-                        {airport.name}
-                        <span className="rating">
-                        {airport.rating}
-                        </span>
-                        </span>
-                       
-                        <span className="code">
-                        Code: {airport.code}
-                        </span>
-                    
-                        </div>)
+            <div className="dateSelect">
+                {dates.map((date, index)=>{
+                    return(
+                        <div className="date" name={date.active?'active':'' } onClick={()=>{setActiveDate(index)}}>
+                            {date.date} {date.month}
+                        </div>
+                    )
                 })}
-           </div>
-        </div>
+            </div>
+        </>
     )
 }
 
